e8498b583b53c81d0a7f4898e1ed123c4c541c50
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-06-19 09:19:39 -0400

    connect with RESTFUL API and refactor code display stream function

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,59 @@
-import React from 'react';
+//Refactor to StreamForm
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { fetchStream, editStream } from '../../actions';
+//
+// class StreamEdit extends React.Component{
+//   componentDidMount(){
+//     this.props.fetchStream(this.props.match.params.id);
+//   }
+//
+//   render() {
+//     if(!this.props.stream){
+//       return <div>Loading</div>
+//     }
+//     return (
+//     <div>{this.props.stream.title}</div>
+//   )
+//   }
+// };
+//
+// const mapStateToProps = (state, ownProps) => {
+//   return {stream: state.streams[ownProps.match.params.id] };
+// }
+//
+// export default connect(mapStateToProps, {fetchStream})(StreamEdit);
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+
+class StreamEdit extends React.Component{
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if(!this.props.stream){
+      return <div>Loading</div>
+    }
+    return (
+    <div>
+      <h3>Editing Stream</h3>
+      <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit} />
+    </div>
+  )
+  }
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+//npm install redux-form@8.1.0
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDom.render(<App />, document.querySelector('#root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(reduxThunk)
+  ));
+
+ReactDom.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
